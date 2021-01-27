f7187131c2b54ac1343a38a8620e977f92999f83
Don Lair <don.lair@gmail.com>
2021-01-27 17:17:57 -0500

    Correcting filename reference.

--- apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
+++ apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
@@ -345,6 +345,6 @@ add_action( 'plugins_loaded', __NAMESPACE__ . '\load_coming_soon' );
  * What's New section of the Tools menu
  */
 function load_whats_new() {
-	require_once __DIR__ . '/whats-new/whats-new.php';
+	require_once __DIR__ . '/whats-new/class-whats-new.php';
 }
 add_action( 'plugins_loaded', __NAMESPACE__ . '\load_whats_new' );
